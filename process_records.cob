fb5651a0b330259516dfc73ce8b4cb54e40213f8
Anna O <ajoy0112@outlook.com>
2026-06-15 12:32:59 -0700

    Added records db & enrollments table

--- process_records.cob
+++ process_records.cob
@@ -14,14 +14,23 @@
        DATE-WRITTEN. 06/14/26.
        DATE-COMPILED. 06/14/26.
 
+      * *****************************************************************
+
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SOURCE-COMPUTER. IBM-3081.
        OBJECT-COMPUTER. IBM-3081.
+      * *****************************************************************
 
        DATA DIVISION.
+       WORKING-STORAGE SECTION. 
+       01 ws-name PIC A(30) VALUE 'anna olson'.
+       01 ws-phone PIC 9(10) VALUE '2535146144'.
+       01 ws-voucher PIC x(5) VALUE 'A1234'.
+      * *****************************************************************
 
        PROCEDURE DIVISION.
-           DISPLAY 'HELLO WORLD'.
+           DISPLAY ws-name.
            STOP RUN.
-           
+
+      * *****************************************************************
